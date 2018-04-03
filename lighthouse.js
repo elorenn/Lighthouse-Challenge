76f32f277993f5c9c6f0b899c6f8bc6f5db3b186
Lorena Sarasua <lorena.sarasua@gmail.com>
2018-04-03 01:50:51 -0400

    challenge 3

--- lighthouse.js
+++ lighthouse.js
@@ -38,4 +38,22 @@ function totalCells() {
 
 totalCells();
 
+/* Okay, let's get our lighthouse actually doing some work! 
+As the lighthouse keeper, sometimes you'll need to shine the light 
+directly on a particular cell in the grid, to alert passing ships to 
+a danger there. Your job is to write a function called lightCell() 
+that takes in the coordinates in the form of 'A3' or 'J9' and returns 
+the contents of that specific cell. (Ex: lightCell('B4'); would return "") */
+
+function lightCell(coordinates) {
+  let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  let cordX = coordinates.split("")[0].toLowerCase(); 
+  let cordY = coordinates.split("")[1] - 1; 
+  let y = GRID[cordY];
+  let x = alphabet.indexOf(cordX);
+  let answer = y[x];
+  return answer;
+}
+
+lightCell('B4');
 
